c56844ac99378de01142f10f87baecd482cdcc8e
Glitch (glitch-hello-website) <none>
2022-03-05 22:08:51 +0000

    🌓🐪 Checkpoint ./script.js:42847714/191

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
-var pattern = [2,2,4,3,2,1,2,4];
+var pattern = [];
+for (let i = 0; i < 8; ++i){
+  pattern[i] = Math.floor(Math.random() * (4 - 1 + 1) + 1);
+}
 var progress = 0;
-var gr
+var gamePlaying = false;
+
+function startGame
