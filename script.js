bc088b73b79411a8f13ae39a9dc05f32b28a2a7b
Glitch (glitch-hello-website) <none>
2022-03-05 22:42:59 +0000

    🚲🌚 Checkpoint ./index.html:42847714/250 ./script.js:42847714/1009 ./style.css:42847714/44

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 var pattern = [];
 var progress = 0;
 var gamePlaying = false;
+var tonePlaying = false;
+var volume = 0.5;
 
 function generatePattern(){
   for (let i = 0; i < 8; ++i){
@@ -27,3 +29,47 @@ function stopGame(){
   document.getElementById("stopBtn").classList.add("hidden");
   document.getElementById("startBtn").classList.remove("hidden");
 }
+
+// Sound Synthesis Functions
+const freqMap = {
+  1: 261.6,
+  2: 329.6,
+  3: 392,
+  4: 466.2
+}
+
+function playTone(btn,len){ 
+  o.frequency.value = freqMap[btn]
+  g.gain.setTargetAtTime(volume,context.currentTime + 0.05,0.025)
+  context.resume()
+  tonePlaying = true
+  setTimeout(function(){
+    stopTone()
+  },len)
+}
+
+function startTone(btn){
+  if(!tonePlaying){
+    context.resume()
+    o.frequency.value = freqMap[btn]
+    g.gain.setTargetAtTime(volume,context.currentTime + 0.05,0.025)
+    context.resume()
+    tonePlaying = true
+  }
+}
+
+function stopTone(){
+  g.gain.setTargetAtTime(0,context.currentTime + 0.05,0.025)
+  tonePlaying = false
+}
+
+// Page Initialization
+// Init Sound Synthesizer
+var AudioContext = window.AudioContext || window.webkitAudioContext 
+var context = new AudioContext()
+var o = context.createOscillator()
+var g = context.createGain()
+g.connect(context.destination)
+g.gain.setValueAtTime(0,context.currentTime)
+o.connect(g)
+o.start(0)
