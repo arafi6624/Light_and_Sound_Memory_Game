5e260219df0aa56c0d3185872636f08cd0d8faa8
Glitch (achieved-island-end) <none>
2022-03-13 02:02:47 +0000

    🍭📰 Checkpoint ./script.js:42847714/1170 ./index.html:42847714/5

--- script.js
+++ script.js
@@ -14,41 +14,41 @@ var cluePauseTime = 333; // How long to pause in between clues
 var attempts = 0; // Counts the number of attempts 
 
 /**
-* Generates a new random pattern for each game
+* Generates a new random pattern for each game.
 */
 function generatePattern(){
   for (let i = 0; i < 8; ++i){
-  pattern[i] = Math.floor(Math.random() * (5 - 1 + 1) + 1); //Generates a random number between 1 and 5 and stores it in the i-th elemenent of pattern
+  pattern[i] = Math.floor(Math.random() * (5 - 1 + 1) + 1); // Generates a random number between 1 and 5 and stores it in the i-th elemenent of pattern.
   }
 }
 
 /**
-* Initializes the game variables and starts the game
+* Initializes the game variables and starts the game.
 */
 function startGame(){
-  //initialize game variables
-  progress = 0;
-  gamePlaying = true;
-  cluePauseTime = 333;
-  clueHoldTime = 1000;
-  attempts = 0;
-  generatePattern();
+  // Initializes game variables
+  progress = 0; // Progress is reset.
+  gamePlaying = true; // gamePlaying is set to true.
+  cluePauseTime = 333; // CluePauseTime is reset.
+  clueHoldTime = 1000; // ClueHoldTime is reset.
+  attempts = 0; // Attempts are reset.
+  generatePattern(); // A new pattern is generated.
   
-  //swap the Start and Stop buttons
+  // Swaps the Start and Stop buttons
   document.getElementById("startBtn").classList.add("hidden");
   document.getElementById("stopBtn").classList.remove("hidden");
   
-  playClueSequence();
+  playClueSequence(); // A clue sequence is played.
 }
 
 /**
-* Stops the game
+* Stops the game.
 */
 function stopGame(){
-  //initialize game variables
-  gamePlaying = false;
+  // Initialize game variables
+  gamePlaying = false; // gamePlaying is set to false.
   
-  //swap the Stop and Start buttons
+  // Swaps the Stop and Start buttons
   document.getElementById("stopBtn").classList.add("hidden");
   document.getElementById("startBtn").classList.remove("hidden");
 }
@@ -63,7 +63,7 @@ const freqMap = {
 }
 
 /**
-* Plays a tone
+* Plays a tone.
 */
 function playTone(btn,len){ 
   o.frequency.value = freqMap[btn]
@@ -76,7 +76,7 @@ function playTone(btn,len){
 }
 
 /**
-* Starts the tone 
+* Starts the tone.
 */
 function startTone(btn){
   if(!tonePlaying){
@@ -89,7 +89,7 @@ function startTone(btn){
 }
 
 /**
-* Stops the tone
+* Stops the tone.
 */
 function stopTone(){
   g.gain.setTargetAtTime(0,context.currentTime + 0.05,0.025)
@@ -108,21 +108,21 @@ o.connect(g)
 o.start(0)
 
 /**
-* Lights up the buttons by changing its color
+* Lights up the buttons by changing its color.
 */
 function lightButton(btn){
-  document.getElementById("btn"+btn).classList.add("lit")
+  document.getElementById("btn"+btn).classList.add("lit") 
 }
 
 /**
-* Changes the button's color to original
+* Changes the button's color to original.
 */
 function clearButton(btn){
   document.getElementById("btn"+btn).classList.remove("lit")
 }
 
 /**
-* Plays a single clue
+* Plays a single clue.
 */
 function playSingleClue(btn){
   if(gamePlaying){
@@ -133,65 +133,73 @@ function playSingleClue(btn){
 }
 
 /**
-* Plays a sequence of clues based on the pattern and level of progress
+* Plays a sequence of clues based on the pattern and level of progress.
 */
 function playClueSequence(){
   context.resume()
   guessCounter = 0;
-  let delay = nextClueWaitTime; //set delay to initial wait time
-  for(let i=0;i<=progress;i++){ // for each clue that is revealed so far
+  let delay = nextClueWaitTime; // Set delay to initial wait time.
+  for(let i=0;i<=progress;i++){ // For each clue that is revealed so far.
     console.log("play single clue: " + pattern[i] + " in " + delay + "ms")
-    setTimeout(playSingleClue,delay,pattern[i]) // set a timeout to play that clue
+    setTimeout(playSingleClue,delay,pattern[i]) // Set a timeout to play that clue.
     delay += clueHoldTime 
     delay += cluePauseTime;
   }
 }
 
 /**
-* Takes in 
+* Takes in the button press as a user input and progresses the game if the guess is correct, otherwise ends the game.
 */
 function guess(btn){
-  console.log("user guessed: " + btn);
+  console.log("user guessed: " + btn); // The user's guess is printed to the console.
+  
+  // If game isn't being played the function returns nothing.
   if(!gamePlaying){
-    return;
+    return; 
   }
   
-  // add game logic here
+  // Game logic
   if(pattern[guessCounter] == btn){
-    //Correct guess
+    // Correct guess
     if(guessCounter == progress){
       if(progress == pattern.length - 1){
-        winGame(); //Game won
+        winGame(); // Game won
       }else{
-        //Pattern was correct, next segment is added
+        // Pattern was correct, next segment is added
         ++progress;
         if(cluePauseTime > 150){
-          cluePauseTime -= 50; //Clue pause time is decremented after each successful level if it's above 150
+          cluePauseTime -= 50; // Clue pause time is decremented after each successful level if it's above 150.
         }
         if(clueHoldTime > 300){
-          clueHoldTime -= 75; //Clue hold time is decremented after each successful level if it's above 300
+          clueHoldTime -= 75; // Clue hold time is decremented after each successful level if it's above 300.
         }
   
-        playClueSequence();
+        playClueSequence(); // The clue sequence is played.
       }
     }else{
-      ++guessCounter; 
+      ++guessCounter; // guessCounter is incremented.
     }
   }else if(attempts < 2){
-    ++attempts;
-    alert("You have " + (3-attempts) + " attempts remaining! Try again after the clue.");
-    guessCounter = 0;
-    playClueSequence();
+    ++attempts; // Increments the number of attemtps.
+    alert("You have " + (3-attempts) + " attempts remaining! Try again after the clue."); // Alerts the user of the of the number of attempts left. 
+    guessCounter = 0; // guessCounter is reset.
+    playClueSequence(); // Clue sequence is replayed.
   }else{
-    loseGame();
+    loseGame(); // Game is lost.
   }
 }
 
+/**
+* Stops the game if the game is lost and alerts the user.
+*/
 function loseGame(){
-  stopGame();
+  stopGame(); 
   alert("Game Over. You lost.");
 }
 
+/**
+* Stops the game if the user won and alerts the user.
+*/
 function winGame(){
   stopGame();
   alert("You won!")
